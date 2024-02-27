b86f49f1bdf3912e5efaa0566f97eeba94a4b991
park <qkr970504@gmail.com>
2024-02-28 08:50:46 +0900

    Refactor - cardDeck, card 책임 분리

--- src/main/java/org/blackjack/domain/Card.java
+++ src/main/java/org/blackjack/domain/Card.java
@@ -14,10 +14,33 @@ import lombok.Setter;
 public class Card {
     private String pattern;
     private String denomination;
+    private int point;
 
-    public Card(String pattern, String denomination) {
+    public Card(String pattern, int index) {
         this.pattern = pattern;
-        this.denomination = denomination;
+        this.denomination = this.numberToDenomination(index);
+        this.point = this.numberToPoint(index);
+    }
+
+    private String numberToDenomination(int number) {
+        if (number == 1) {
+            return "A";
+        } else if (number == 11) {
+            return "J";
+        } else if (number == 12) {
+            return "Q";
+        } else if (number == 13) {
+            return "K";
+        }
+
+        return String.valueOf(number);
+    }
+
+    private int numberToPoint(int number) {
+        if (number >= 11) {
+            return 10;
+        }
+        return number;
     }
 
     @Override
--- src/main/java/org/blackjack/domain/CardDeck.java
+++ src/main/java/org/blackjack/domain/CardDeck.java
@@ -42,28 +42,12 @@ public class CardDeck {
 
         for (String pattern : PATTERNS) {
             for (int i = 1; i < CARD_COUNT; i++) {
-                String denomination = numberToDenomination(i);
-                Card card = new Card(pattern, denomination);
-
+                Card card = new Card(pattern, i);
                 cards.add(card);
             }
         }
 
         return cards;
-    }
-
-    private static String numberToDenomination(int number) {
-        if (number == 1) {
-            return "A";
-        } else if (number == 11) {
-            return "J";
-        } else if (number == 12) {
-            return "Q";
-        } else if (number == 13) {
-            return "K";
-        }
-
-        return String.valueOf(number);
     }
 
     @Override
