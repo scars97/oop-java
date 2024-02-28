cecaec3e2905b70f8d410c0db7a37b5c2e9d3065
park <qkr970504@gmail.com>
2024-02-28 09:02:19 +0900

    Game - dealer 추가

--- src/main/java/org/blackjack/Game.java
+++ src/main/java/org/blackjack/Game.java
@@ -21,27 +21,44 @@ public class Game {
         Rule rule = new Rule();
         CardDeck cardDeck = new CardDeck();
 
-        initPhase(cardDeck, gamer);
-        playingPhase(sc, cardDeck, gamer);
+        initPhase(cardDeck, gamer, dealer);
+        playingPhase(sc, cardDeck, gamer, dealer);
     }
 
-    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer) {
-        String gameInput;
+    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer, Dealer dealer) {
+        String gameInput, dealerInput;
+        boolean isGamerTurn = false,
+                isDealerTurn = false;
+
         while (true) {
             System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
             gameInput = sc.nextLine();
 
             if ("0".equals(gameInput)) {
-                break;
+                isGamerTurn = true;
+            } else {
+                Card card = cardDeck.draw();
+                gamer.receiveCard(card);
             }
 
-            Card card = cardDeck.draw();
-            gamer.receiveCard(card);
+            System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
+            dealerInput = sc.nextLine();
+
+            if ("0".equals(dealerInput)) {
+                isDealerTurn = true;
+            } else {
+                Card card = cardDeck.draw();
+                dealer.receiveCard(card);
+            }
+
+            if (isGamerTurn && isDealerTurn) {
+                break;
+            }
         }
     }
 
     // 처음 시작시 Dealer와 Gamer가 2장씩 카드를 받는 역할
-    private void initPhase(CardDeck cardDeck, Gamer gamer) {
+    private void initPhase(CardDeck cardDeck, Gamer gamer, Dealer dealer) {
         System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
         // 매직너버를 피하기 위한 static 상수 사용
         // 매직넘버 : 정체를 알 수 없지만 특정 기능을 하는 숫자
@@ -49,6 +66,9 @@ public class Game {
         for (int i = 0; i < INIT_RECEIVE_CARD_COUNT; i++) {
             Card card = cardDeck.draw();
             gamer.receiveCard(card);
+
+            Card card2 = cardDeck.draw();
+            dealer.receiveCard(card2);
         }
     }
 
