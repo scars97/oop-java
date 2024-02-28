612b20c6665a5498ef63b077f18871c3a2e70857
park <qkr970504@gmail.com>
2024-02-28 09:28:37 +0900

    Player 추상화로 인한 Game 로직 수정

--- src/main/java/org/blackjack/Game.java
+++ src/main/java/org/blackjack/Game.java
@@ -2,6 +2,8 @@ package org.blackjack;
 
 import org.blackjack.domain.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -10,66 +12,73 @@ import java.util.Scanner;
 public class Game {
 
     private static final int INIT_RECEIVE_CARD_COUNT = 2;
+    private static final String STOP_RECEIVE_CARD = "0";
 
     public void play() {
         System.out.println("========= Blackjack =========");
         Scanner sc = new Scanner(System.in);
-
-        // 클래스의 인스턴스 생성
-        Dealer dealer = new Dealer();
-        Gamer gamer = new Gamer();
         Rule rule = new Rule();
         CardDeck cardDeck = new CardDeck();
 
-        initPhase(cardDeck, gamer, dealer);
-        playingPhase(sc, cardDeck, gamer, dealer);
+        List<Player> players = Arrays.asList(new Gamer("사용자1"), new Dealer());
+        List<Player> initAfterPlayers = initPhase(cardDeck, players);
+        List<Player> playingAfterPlayers = playingPhase(sc, cardDeck, initAfterPlayers);
+
+        Player winner = rule.getWinner(playingAfterPlayers);
+        System.out.println("승자는 " + winner.getName());
     }
 
-    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer, Dealer dealer) {
-        String gameInput, dealerInput;
-        boolean isGamerTurn = false,
-                isDealerTurn = false;
-
+    private List<Player> playingPhase(Scanner sc, CardDeck cardDeck, List<Player> players) {
+        List<Player> cardReceivedPlayers;
         while (true) {
-            System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
-            gameInput = sc.nextLine();
+            cardReceivedPlayers = receiveCardAllPlayers(sc, cardDeck, players);
 
-            if ("0".equals(gameInput)) {
-                isGamerTurn = true;
-            } else {
-                Card card = cardDeck.draw();
-                gamer.receiveCard(card);
-            }
-
-            System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
-            dealerInput = sc.nextLine();
-
-            if ("0".equals(dealerInput)) {
-                isDealerTurn = true;
-            } else {
-                Card card = cardDeck.draw();
-                dealer.receiveCard(card);
-            }
-
-            if (isGamerTurn && isDealerTurn) {
+            if (isAllPlayerTurnOff(cardReceivedPlayers)) {
                 break;
             }
         }
+        return cardReceivedPlayers;
+    }
+
+    private List<Player> receiveCardAllPlayers(Scanner sc, CardDeck cardDeck, List<Player> players) {
+        for (Player player : players) {
+            if (isReceiveCard(sc)) {
+                Card card = cardDeck.draw();
+                player.receiveCard(card);
+                player.turnOn();
+            } else {
+                player.turnOff();
+            }
+        }
+
+        return players;
+    }
+
+    private boolean isAllPlayerTurnOff(List<Player> players) {
+        for (Player player : players) {
+            if (player.isTurn()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isReceiveCard(Scanner sc) {
+        System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
+        return !STOP_RECEIVE_CARD.equals(sc.nextLine());
     }
 
     // 처음 시작시 Dealer와 Gamer가 2장씩 카드를 받는 역할
-    private void initPhase(CardDeck cardDeck, Gamer gamer, Dealer dealer) {
+    private List<Player> initPhase(CardDeck cardDeck, List<Player> players) {
         System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
         // 매직너버를 피하기 위한 static 상수 사용
-        // 매직넘버 : 정체를 알 수 없지만 특정 기능을 하는 숫자
-        // 매직넘버 사용시 의미가 모호해지며 변경범위를 확인하기 어렵다.
         for (int i = 0; i < INIT_RECEIVE_CARD_COUNT; i++) {
-            Card card = cardDeck.draw();
-            gamer.receiveCard(card);
-
-            Card card2 = cardDeck.draw();
-            dealer.receiveCard(card2);
+            for (Player player : players) {
+                Card card = cardDeck.draw();
+                player.receiveCard(card);
+            }
         }
-    }
 
+        return players;
+    }
 }
