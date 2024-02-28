e143d1ab6a4f99a05a0fe5e76dff795ae5275327
park <qkr970504@gmail.com>
2024-02-28 12:44:18 +0900

    Rule 로직 추가

--- src/main/java/org/blackjack/domain/Rule.java
+++ src/main/java/org/blackjack/domain/Rule.java
@@ -9,11 +9,27 @@ import java.util.List;
  * 승패를 판단한다.
  */
 public class Rule {
-    public int getScore(List<Card> cards) {
-        return 0;
+
+    public Player getWinner(List<Player> players) {
+        Player highestPlayer = null;
+        int highestPoint = 0;
+
+        for (Player player : players) {
+            int playerPointSum = getPointSum(player.openCards());
+            if (playerPointSum > highestPoint) {
+                highestPlayer = player;
+                highestPoint = playerPointSum;
+            }
+        }
+        return highestPlayer;
     }
 
-    public void getWinner(Dealer dealer, Gamer gamer) {
+    private int getPointSum(List<Card> cards) {
+        int sum = 0;
 
+        for (Card card : cards) {
+            sum += card.getPoint();
+        }
+        return sum;
     }
 }
