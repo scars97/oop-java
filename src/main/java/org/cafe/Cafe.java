276094fa4f8f229abc375827f4cd140a8f52846a
park <qkr970504@gmail.com>
2024-03-12 18:12:09 +0900

    [코드구현] showMenu 추가

--- src/main/java/org/cafe/Cafe.java
+++ src/main/java/org/cafe/Cafe.java
@@ -2,34 +2,16 @@ package org.cafe;
 
 import org.cafe.domain.Barista;
 import org.cafe.domain.Cashier;
-import org.cafe.domain.menu.Coffee;
 import org.cafe.domain.menu.Menu;
-import org.cafe.domain.menu.Tea;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Cafe {
 
     public void open() {
         System.out.println("========= Cafe =========");
+        Menu menus = new Menu();
         Cashier cashier = new Cashier();
         Barista barista = new Barista();
 
-        List<Menu> coffeeList = Arrays.asList(
-            new Coffee("C01", "아메리카노", 1500),
-            new Coffee("C02", "카페라떼", 2900),
-            new Coffee("C03", "카페모카", 3200),
-            new Coffee("C04", "바닐라라떼", 3200)
-        );
-
-        List<Menu> teaList = Arrays.asList(
-                new Tea("T01", "캐모마일", 2500),
-                new Tea("T02", "얼그레이", 2500),
-                new Tea("T03", "유자차", 2300)
-        );
-
-
+        menus.showMenu();
     }
 }
--- src/main/java/org/cafe/domain/menu/Menu.java
+++ src/main/java/org/cafe/domain/menu/Menu.java
@@ -1,9 +1,71 @@
 package org.cafe.domain.menu;
 
-import java.util.List;
+import lombok.Getter;
 
-public interface Menu {
+@Getter
+public class Menu {
 
-    int getPrice();
+    public void showMenu() {
+        StringBuilder sb = new StringBuilder();
 
+        sb.append("-------- Coffee --------\n");
+        Coffee[] coffeeList = Coffee.values();
+        for (Coffee coffees : coffeeList) {
+            String coffeeString = "[" + coffees.cd + "] " + coffees.name + " " + coffees.price + "\n";
+            sb.append(coffeeString);
+        }
+
+        sb.append("-------- Tea --------\n");
+        Tea[] teaList = Tea.values();
+        for (Tea teas : teaList) {
+            String teaString = "[" + teas.cd + "] " + teas.name + " " + teas.price + "\n";
+            sb.append(teaString);
+        }
+
+        System.out.println(sb);
+    }
+
+    public void withoutMenu(String cd) {
+        try {
+            Coffee order = Coffee.valueOf(cd);
+            System.out.println(order.getName());
+        } catch (IllegalArgumentException e) {
+            System.out.println("없는 메뉴");
+        }
+    }
+
+    @Getter
+    public enum Coffee {
+        C01("C01", "아메리카노", 1500),
+        C02("C02", "카페라떼", 2900),
+        C03("C03", "카페모카", 3300),
+        C04("C04", "바닐라라떼", 3300);
+
+        private String cd;
+        private String name;
+        private Integer price;
+
+        Coffee(String cd, String name, Integer price) {
+            this.cd = cd;
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    @Getter
+    public enum Tea {
+        T01("T01", "캐모마일", 2500),
+        T02("T02", "아메리카노", 2500),
+        T03("T03", "유자차", 2300);
+
+        private String cd;
+        private String name;
+        private Integer price;
+
+        Tea(String cd, String name, Integer price) {
+            this.cd = cd;
+            this.name = name;
+            this.price = price;
+        }
+    }
 }
