85d82af298c0077aa582682aec909bf020f57418
park <qkr970504@gmail.com>
2024-03-15 14:58:47 +0900

    [코드구현] Customer 스펙 아웃 & Orders로 변경

--- src/main/java/org/cafe/Cafe.java
+++ src/main/java/org/cafe/Cafe.java
@@ -11,13 +11,12 @@ public class Cafe {
         System.out.println("========= Cafe =========");
         Scanner sc = new Scanner(System.in);
 
-        Customer customer = new Customer();
         Cashier cashier = new Cashier();
         Barista barista = new Barista();
         Orders orders = new Orders();
 
         showMenu();
-        facing(sc, customer, orders);
+        facing(sc, orders);
     }
 
     public void showMenu() {
@@ -33,9 +32,10 @@ public class Cafe {
         System.out.println(sb);
     }
 
-    public void facing(Scanner sc, Customer customer, Orders orders) {
+    // 고객 응대
+    public void facing(Scanner sc, Orders orders) {
         String customerInput;
-        int menuCnt = customer.getMenus().size();
+        int menuCnt = orders.getMenuList().size();
 
         while (true) {
             System.out.println("주문하실 메뉴를 선택해주세요. ex) C01 \n종료를 원하시면 0을 입력하세요.");
@@ -44,17 +44,18 @@ public class Cafe {
             if ("0".equals(customerInput)) {
                 if (menuCnt == 0) {
                     System.out.println("이용해주셔서 감사합니다.");
-                    break;
                 } else {
-                    String orderMenus = orders.showMenus(customer.getMenus());
-                    System.out.println("현재 주문 내역: \n" + orderMenus);
-                    break;
+                    // 주문내역
+                    String orderMenus = orders.showMenus();
+                    int totalPayment = orders.payment();
+                    System.out.println("현재 주문 내역: \n" + orderMenus + "\n총 결제 금액: " + totalPayment);
                 }
+                break;
             }
 
             if (isMenu(customerInput)) {
                 Menu pickMenu = Menu.valueOf(customerInput);
-                List<Menu> menus = customer.pickMenu(pickMenu);
+                List<Menu> menus = orders.addMenu(pickMenu);
                 menuCnt = menus.size();
             } else {
                 System.out.println("없는 메뉴입니다.");
--- src/main/java/org/cafe/domain/Orders.java
+++ src/main/java/org/cafe/domain/Orders.java
@@ -2,6 +2,7 @@ package org.cafe.domain;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -9,19 +10,32 @@ public class Orders {
 
     private List<Menu> menuList;
 
+    public Orders() {
+        this.menuList = new ArrayList<>();
+    }
+
     public List<Menu> addMenu(Menu order) {
         this.menuList.add(order);
 
         return this.menuList;
     }
 
-    public String showMenus(List<Menu> menus) {
+    public String showMenus() {
         StringBuilder sb = new StringBuilder();
 
-        for (Menu menu : menus) {
+        for (Menu menu : this.menuList) {
             sb.append(menu.getName() + " ");
         }
 
         return sb.toString();
+    }
+
+    public int payment() {
+        int price = 0;
+        for (Menu menu : this.menuList) {
+            price += menu.getPrice();
+        }
+
+        return price;
     }
 }
