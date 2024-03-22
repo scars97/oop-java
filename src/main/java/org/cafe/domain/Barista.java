ddc38cf56556956bd9cc56e3af32588b4563b643
park <qkr970504@gmail.com>
2024-03-22 18:02:43 +0900

    [구현] 바리스타 구현

--- src/main/java/org/cafe/domain/Barista.java
+++ src/main/java/org/cafe/domain/Barista.java
@@ -1,5 +1,6 @@
 package org.cafe.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,5 +9,28 @@ import java.util.List;
  * 제작된 음료를 고객에게 전달한다.
  */
 public class Barista {
-    private Orders orders;
+    private List<Menu> completion;
+
+    public List<Menu> manufacturing(Orders orders) {
+        List<Menu> orderList = orders.getMenuList();
+
+        for (Menu menu : orderList) {
+            String menuName = menu.getName();
+
+            try {
+                Thread.sleep(2000);
+                System.out.println(menuName + " 완료");
+
+                completion.add(menu);
+            } catch (InterruptedException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        return completion;
+    }
+
+    public Barista() {
+        this.completion = new ArrayList<>();
+    }
 }
